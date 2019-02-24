ab5593c8894f1e0e0f347bd56b0aadc3ca83cfdc
Chad Fawcett <me@chadf.ca>
2019-02-23 17:46:56 -0800

    add app creator to tests

--- index.test.js
+++ index.test.js
@@ -5,9 +5,18 @@ const { serverless } = require('./')
 nock.disableNetConnect()
 nock.enableNetConnect('127.0.0.1')
 
+const createApp = name => {
+  const route = `/${name}`
+  return [
+    route,
+    app =>
+      app.route(route).get('/', (_, res) => res.send(`Hello from ${route}`))
+  ]
+}
+
 test('exports regular NodeJS listener usable by servers', () => {
-  const defaultApp = require('probot/lib/apps/default')
-  return request(serverless(defaultApp, { githubToken: 'faketoken' }))
-    .get('/probot')
+  const [route, app] = createApp('app1')
+  return request(serverless(app, { githubToken: 'faketoken' }))
+    .get(route)
     .expect(200)
 })
