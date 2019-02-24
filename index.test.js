5c81539bdce6521880df2b56517fd7e7f3433b10
Chad Fawcett <me@chadf.ca>
2019-02-23 18:00:51 -0800

    Test multiple apps

--- index.test.js
+++ index.test.js
@@ -20,3 +20,16 @@ test('exports regular NodeJS listener usable by servers', () => {
     .get(route)
     .expect(200)
 })
+
+test('accepts an array of apps', async () => {
+  const [route1, app1] = createApp('app1')
+  const [route2, app2] = createApp('app2')
+  const bot = serverless([app1, app2], { githubToken: 'faketoken' })
+
+  await request(bot)
+    .get(route1)
+    .expect(200, `Hello from ${route1}`)
+  await request(bot)
+    .get(route2)
+    .expect(200, `Hello from ${route2}`)
+})
